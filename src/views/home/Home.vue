7b42c9374c359f68eb73e71ad289cae2ba27a55b
jimmywong <jameswangcnbj@gmail.com>
2025-01-13 17:58:11 +0800

    '首页推荐'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,19 +2,20 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"/>
-
+    <home-recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
   import NavBar from '@/components/common/navbar/NavBar'
-  import HomeSwiper from '@/views/home/childComps/HomeSwiper'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import HomeRecommendView from './childComps/HomeRecommendView'
   import {getHomeMultiData} from "@/network/home";
 
 
   export default {
     name: "Home",
-    components: {NavBar,HomeSwiper},
+    components: {NavBar,HomeSwiper,HomeRecommendView},
     data(){
       return {
         banners:[],
@@ -28,7 +29,7 @@
           console.log(res)
           // this.banners = res._data._rows
           this.banners = res.data.banner.list
-          // this.recommends = res.data.recommend.list
+          this.recommends = res.data.recommend.list
         })
 
     }
---
+++ src/views/home/childComps/HomeRecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="recommend">
+      <div v-for="item in recommends" class="recommend-item">
+        <a :href="item.link" target="_blank">
+          <img :src="item.image" >
+          <div>{{ item.title }}</div>
+        </a>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeRecommendView",
+        props:{
+          recommends:{
+            type: Array,
+            default(){
+              return []
+            }
+          }
+        }
+    }
+</script>
+
+<style scoped>
+.recommend {
+    display: flex;
+    margin-top: 10px;
+    font-size: 14px;
+    padding-bottom: 15px;
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+    text-align: center;
+  }
+
+  .recommend img {
+    width: 80px;
+    height: 80px;
+    margin-bottom: 5px;
+  }
+</style>
